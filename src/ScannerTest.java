7a6f8b6c1b3f8d5100f49cc59ac1040b37e05378
EWCMF <TGHpublic@hotmail.com>
2019-09-09 10:49:54 +0200

    Fix name

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class scannerTest {
+class ScannerTest {
     public static void main(String[] args) {
         Scanner myVar = new Scanner(System.in);
         System.out.println("You printed " + myVar.nextLine());
