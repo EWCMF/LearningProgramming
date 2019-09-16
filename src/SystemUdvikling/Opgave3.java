e92047a175ac53d6fbd39a707a37bcb31be0ab29
EWCMF <TGHpublic@hotmail.com>
2019-09-16 20:28:13 +0200

    System udvikling opgave 3 og 4

---
+++ src/SystemUdvikling/Opgave3.java
@@ -0,0 +1,47 @@
+package SystemUdvikling;
+
+public class Opgave3 {
+    public static void main(String[] args) {
+        // Test case for hvilken er størst hvor second er størst
+        // input: first = 41, second = 55
+        // Forventet output: test == 2
+
+        // opstil input
+        int first = 41;
+        int second = 55;
+
+        // Generer output
+        int test = findLargest(first, second);
+
+        // Lav sammenligning
+        if (test == 2)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+
+        // Test case for hvilken er størst hvor first er størst
+        // input: first = 66, second = 11
+        // Forventet output: test == 1
+
+        first = 66;
+        second = 11;
+        test = findLargest(first, second);
+
+        if (test == 1)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+
+    // Metode uden for test
+    static int findLargest(int a, int b) {
+        if (a > b) {
+            System.out.println(a + " is the largest");
+            return 1;
+        }
+        else {
+            System.out.println(b + " is the largest");
+            return 2;
+        }
+    }
+}
---
+++ src/SystemUdvikling/Opgave4.java
@@ -0,0 +1,75 @@
+package SystemUdvikling;
+
+public class Opgave4 {
+    public static void main(String[] args) {
+        // Test case for hvilken er størst hvor third er størst
+        // input: first = 31, second = 45, third = 99
+        // Forventet output: test == 3
+
+        // Opstil input
+        int first = 31;
+        int second = 45;
+        int third = 99;
+
+        // Generer output
+        int test = findLargest(first, second, third);
+
+        // Lav sammenligning
+        if (test == 3)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+
+        // Test case for hvilken er størst hvor first er størst
+        // input: first = 5, second = 1, third = 3
+        // Forventet output: test == 1
+
+        // Opstil input
+        first = 5;
+        second = 1;
+        third = 3;
+
+        // Generer output
+        test = findLargest(first, second, third);
+
+        if (test == 1)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+
+        // Test case for hvilken er størst hvor second er størst
+        // input: first = 100, second = 1000, third = 10
+        // Forventet output: test == 2
+
+        // Opstil input
+        first = 100;
+        second = 1000;
+        third = 10;
+
+        // Generer output
+        test = findLargest(first, second, third);
+
+        if (test == 2)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+
+
+    // Metode uden for test
+    static int findLargest(int a, int b, int c) {
+        if (a > b && a > c) {
+            System.out.println(a + " is the largest");
+            return 1;
+        }
+        else if (b > a && b > c) {
+            System.out.println(b + " is the largest");
+            return 2;
+        }
+        else {
+            System.out.println(c + " is the largest");
+            return 3;
+        }
+
+    }
+}
