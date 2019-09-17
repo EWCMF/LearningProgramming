d54aea2e5f014f8e359da7e476ee19c31dfa1d01
EWCMF <TGHpublic@hotmail.com>
2019-09-17 12:15:53 +0200

    System udvikling opgaver fix

--- src/SystemUdvikling/Opgave5.java
+++ src/SystemUdvikling/Opgave5.java
@@ -12,11 +12,9 @@ public class Opgave5 {
         String output = whatTierInterval(x);
 
         if (output == "Tallet tilhører [60:69]") {
-            System.out.println(output);
             System.out.println("Pass");
         }
         else {
-            System.out.println(output);
             System.out.println("Fail");
         }
     }
@@ -25,24 +23,34 @@ public class Opgave5 {
         int compare = a / 10;
         switch (compare) {
             case 0:
+                System.out.println(a + " tilhører [0:9]");
                 return "Tallet tilhører [0:9]";
             case 1:
+                System.out.println(a + " tilhører [10:19]");
                 return "Tallet tilhører [10:19]";
             case 2:
+                System.out.println(a + " tilhører [20:29]");
                 return "Tallet tilhører [20:29]";
             case 3:
+                System.out.println(a + " tilhører [30:39]");
                 return "Tallet tilhører [30:39]";
             case 4:
+                System.out.println(a + " tilhører [40:49]");
                 return "Tallet tilhører [40:49]";
             case 5:
+                System.out.println(a + " tilhører [50:59]");
                 return "Tallet tilhører [50:59]";
             case 6:
+                System.out.println(a + " tilhører [60:69]");
                 return "Tallet tilhører [60:69]";
             case 7:
+                System.out.println(a + " tilhører [70:79]");
                 return "Tallet tilhører [70:79]";
             case 8:
+                System.out.println(a + " tilhører [80:89]");
                 return "Tallet tilhører [80:89]";
             case 9:
+                System.out.println(a + " tilhører [90:99]");
                 return "Tallet tilhører [90:99]";
         }
         return null;
