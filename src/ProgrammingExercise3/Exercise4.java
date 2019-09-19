333b98475cb0bdd0bc97177609b32a2605cb5917
EWCMF <TGHpublic@hotmail.com>
2019-09-19 22:52:36 +0200

    Programmering exercise tilføjelse

--- src/ProgrammingExercise3/Exercise4.java
+++ src/ProgrammingExercise3/Exercise4.java
@@ -8,6 +8,53 @@ public class Exercise4 {
 
         int maaned = (int) (Math.random() * 12) + 1;
 
-        int output = hvadMaaned();
+        int output = hvadMaaned(maaned);
+
+        if (output >= 1 && output <= 12)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+
+    static int hvadMaaned(int maaned) {
+        switch (maaned) {
+            case 1:
+                System.out.println("Januar");
+                return 1;
+            case 2:
+                System.out.println("Februar");
+                return 2;
+            case 3:
+                System.out.println("Marts");
+                return 3;
+            case 4:
+                System.out.println("April");
+                return 4;
+            case 5:
+                System.out.println("Maj");
+                return 5;
+            case 6:
+                System.out.println("Juni");
+                return 6;
+            case 7:
+                System.out.println("Juli");
+                return 7;
+            case 8:
+                System.out.println("August");
+                return 8;
+            case 9:
+                System.out.println("September");
+                return 9;
+            case 10:
+                System.out.println("October");
+                return 10;
+            case 11:
+                System.out.println("November");
+                return 11;
+            case 12:
+                System.out.println("December");
+                return 12;
+        }
+        return 0;
     }
 }
