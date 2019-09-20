ec102aa1bdded9fa806c2a7d5d16c0ee1112f9ac
EWCMF <TGHpublic@hotmail.com>
2019-09-20 13:58:17 +0200

    Læring af superklasse

--- src/Subclasses/Main.java
+++ src/Subclasses/Main.java
@@ -4,25 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Bil jensBil = new Bil();
 
+        // Superklasse variabler
         jensBil.antalHjul = 4;
         jensBil.motor = "Forbrændingsmotor";
         jensBil.gennemsnitshastighed = 123.45;
 
+        // Bilklasse eksklusiv.
         jensBil.antalSaeder = 2;
         jensBil.antalVinduer = 4;
         jensBil.nummerplade = "Roadster";
 
+        // Superklasse metode
         jensBil.travel();
 
         Cykel hansCykel = new Cykel();
 
+        // Superklasse variabler
         hansCykel.antalHjul = 2;
         hansCykel.motor = "Menneskedrevet";
         hansCykel.gennemsnitshastighed = 34.56;
 
+        // Cykelklasse eksklusiv
         hansCykel.cykelBremsetype = "Håndbremse";
         hansCykel.cykelstyrType = "Racerstyr";
 
+        // Superklasse metode
         hansCykel.travel();
     }
 }
