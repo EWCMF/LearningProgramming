e342c487c01792aed5af0c96e9e0738b9cb01fac
EWCMF <TGHpublic@hotmail.com>
2019-09-22 12:50:04 +0200

    Kapitel 3 exercise 14

---
+++ src/ProgrammingExercise3/Exercise14.java
@@ -0,0 +1,30 @@
+package ProgrammingExercise3;
+
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Is it head or tails? (0: tails, 1: heads)");
+        int choice = input.nextInt();
+
+        if (choice < 0 || choice > 1) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        double result = (int) (Math.random() * 100);
+
+        if (result <= 50)
+            result = 0;
+        else
+            result = 1;
+
+        String printResult = (result == 0) ? "Tails" : "Heads";
+        System.out.println("The coin is: " + printResult);
+
+        String winLose = (choice == result) ? "You guessed correct!" : "You guessed wrong!";
+        System.out.println(winLose);
+    }
+}
