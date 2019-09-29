b0e7e27baad54a95c548912b09f80265323c7fb6
EWCMF <TGHpublic@hotmail.com>
2019-09-29 13:26:56 +0200

    Opgaver

--- src/ProgrammingExercise5/Exercise2.java
+++ src/ProgrammingExercise5/Exercise2.java
@@ -1,5 +1,16 @@
 package ProgrammingExercise5;
 
 public class Exercise2 {
-    //søndag
+    public static void main(String[] args) {
+        int celcius = 0;
+        double FAHRENHEIT = celcius * 9 / 5.0 + 32;
+
+        System.out.println("Celcius                Fahrenheit");
+        for (int i = 0; i <= 100; i += 2) {
+            celcius = i;
+            FAHRENHEIT = celcius * 9 / 5.0 + 32;
+            System.out.println(i + "\t\t\t\t\t\t\t" + FAHRENHEIT);
+
+        }
+    }
 }
