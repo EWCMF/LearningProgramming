f87390c5d8926e167379d6ecb13f43b7dd754bcb
Tommy <TGHpublic@hotmail.com>
2019-10-04 14:26:04 +0200

    Opgaver

--- src/ProgrammingExercise5/Exercise5.java
+++ src/ProgrammingExercise5/Exercise5.java
@@ -9,7 +9,7 @@ public class Exercise5 {
         int celcius = 0;
         int fahrenheit = 20;
         while (celcius <= 100) {
-            System.out.println(celcius + "\t\t\t" + ((celcius * 9 / 5.0) + 32) +"\t\t|\t" + fahrenheit + "\t\t\t\t" + ((fahrenheit - 32) * 5.0 / 9));
+            System.out.println(celcius + "\t\t\t" + ((celcius * 9 / 5.0) + 32) +"\t\t|\t" + fahrenheit + "\t\t\t\t" + Math.round(((fahrenheit - 32) * 5.0 / 9) * 1000) / 1000.0);
             celcius += 2;
             fahrenheit +=5;
         }
