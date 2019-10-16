658df4df7fcd144f572e98f894acc4eda2dffee7
Tommy <TGHpublic@hotmail.com>
2019-10-16 20:13:59 +0200

    Metodeopgave 17

--- src/MetodeOpgaver/Opgave17.java
+++ src/MetodeOpgaver/Opgave17.java
@@ -16,6 +16,34 @@ public class Opgave17 {
             output = lige(inputs[i]);
             testOutput(expected[i], output);
         }
+
+        // Test for ulige metode med samme inputs
+        // Input: input = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        // Forventet output: output = false, true, false, true, false, true, false, true, false, true
+
+        // Opstil inputs
+        expected = new boolean[]{true, false, true, false, true, false, true, false, true, false};
+
+        // Generer output
+        for (int i = 0; i < inputs.length; i++) {
+            output = ulige(inputs[i]);
+            testOutput(expected[i], output);
+        }
+
+
+        // Test for swapLigeUlige hvor et byt sker
+        // Input: array = {6, 7, 1, 8, 5}, firstIndex = 0, secondIndex = 1
+        // Forventet output: array = {7, 6, 1, 8, 5}, output = true
+
+        // Opstil input
+        int[] array = {6, 7, 1, 8, 5};
+        int firstIndex = 0;
+        int secondIndex = 1;
+
+        // Generer output
+        output = swapLigeUlige(array, firstIndex, secondIndex);
+
+        testOutput(true, output);
     }
 
     static void testOutput(boolean forventetOutput, boolean faktiskOutput) {
@@ -33,5 +61,13 @@ public class Opgave17 {
         return false;
     }
 
+    static boolean ulige(int tal) {
+        if (tal % 2 != 0)
+            return true;
+        return false;
+    }
 
+    static boolean swapLigeUlige(int[] array, int first, int second) {
+        return false;
+    }
 }
