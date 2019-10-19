ca42fc24df70c384adc7347c5030655918a05798
Tommy <TGHpublic@hotmail.com>
2019-10-19 21:50:29 +0200

    Programming exercise 6.5

--- src/ProgrammingExercise6/Exercise5.java
+++ src/ProgrammingExercise6/Exercise5.java
@@ -1,7 +1,26 @@
 package ProgrammingExercise6;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Exercise5 {
     public static void main(String[] args) {
-        //TODO three numbers decreasing order
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        double num1 = input.nextDouble();
+        System.out.print("Input the second number: ");
+        double num2 = input.nextDouble();
+        System.out.print("Input the third number: ");
+        double num3 = input.nextDouble();
+        displaySortedNumbers(num1, num2, num3);
+    }
+
+    public static void displaySortedNumbers(double num1, double num2, double num3) {
+        Double[] array = {num1, num2, num3};
+
+        Arrays.sort(array, Collections.reverseOrder());
+
+        System.out.println(array[0] + " " + array[1] + " " + array[2]);
     }
 }
