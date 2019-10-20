c63c66c09c4b65123f4446226dcdddfe086f0e3c
Tommy <TGHpublic@hotmail.com>
2019-10-20 09:27:39 +0200

    Programming exercise 6.12

--- src/ProgrammingExercise6/Exercise12.java
+++ src/ProgrammingExercise6/Exercise12.java
@@ -1,4 +1,24 @@
 package ProgrammingExercise6;
 
 public class Exercise12 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 100;
+        int numberPerLine = 10;
+
+        printNumbers(num1, num2, numberPerLine);
+    }
+
+    public static void printNumbers(int num1, int num2, int numberPerLine) {
+        int count = 0;
+        for (int i = num1; i <= num2; i++) {
+            System.out.print((num1) + " ");
+            num1++;
+            count++;
+            if (count == numberPerLine) {
+                count = 0;
+                System.out.println();
+            }
+        }
+    }
 }
