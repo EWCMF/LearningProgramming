9b594195bf6404b6dd6225ba65369ed298c496dd
Tommy <TGHpublic@hotmail.com>
2019-10-26 20:59:58 +0200

    Iteration Opgave 7

--- SystemUdvikling/src/IterationOpgaver/Opgave7.java
+++ SystemUdvikling/src/IterationOpgaver/Opgave7.java
@@ -10,6 +10,17 @@ public class Opgave7 {
     }
 
     static void ligeOgUlige(int min, int max) {
+        int lige = 0;
+        int ulige = 0;
 
+        for (int i = min; i <= max; i++) {
+            if (i % 2 == 0) {
+                lige++;
+            }
+            else {
+                ulige++;
+            }
+        }
+        System.out.println("Der er " + ulige + " ulige og " + lige + " lige tal i intervallet [" + min + ":" + max + "]");
     }
 }
