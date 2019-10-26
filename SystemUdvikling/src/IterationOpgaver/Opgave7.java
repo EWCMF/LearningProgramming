00b2c83f7fd6a8f873e6bfb2671c9078b805b3e8
Tommy <TGHpublic@hotmail.com>
2019-10-26 20:55:01 +0200

    Iteration Opgave 7

---
+++ SystemUdvikling/src/IterationOpgaver/Opgave7.java
@@ -0,0 +1,15 @@
+package IterationOpgaver;
+
+public class Opgave7 {
+    public static void main(String[] args) {
+
+        int minimum = 3;
+        int maximum = 16;
+
+        ligeOgUlige(minimum, maximum);
+    }
+
+    static void ligeOgUlige(int min, int max) {
+
+    }
+}
