7c2f91f926561b6b9adabae7b13cf83714ade6a8
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-11-25 11:07:48 +0100

    fjern overflødig import.

--- ProgrammingExercise/src/ProgrammingExercise9/ProgrammingExercise9Tests.java
+++ ProgrammingExercise/src/ProgrammingExercise9/ProgrammingExercise9Tests.java
@@ -2,8 +2,6 @@ package ProgrammingExercise9;
 
 import junit.framework.TestCase;
 
-import java.util.Scanner;
-
 public class ProgrammingExercise9Tests extends TestCase {
     public void test1() {
         Exercise1.Rectangle r1 = new Exercise1.Rectangle(4, 40);
