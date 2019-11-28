bc7ce0a06e5ad96dda87beaa3749410838a2d28e
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-11-28 22:49:26 +0100

    Programmeringsopgaver kapitel 10.

--- ProgrammingExercise/src/ProgrammingExercise10/Exercise3.java
+++ ProgrammingExercise/src/ProgrammingExercise10/Exercise3.java
@@ -13,55 +13,73 @@ public class Exercise3 {
         }
 
         boolean isEven() {
-            return true;
+            return value % 2 == 0;
         }
 
         boolean isOdd() {
-            return true;
+            return value % 2 != 0;
         }
 
         boolean isPrime() {
+            if (value % 2 == 0)
+                return false;
+            for (int i = 3; i * i <= value; i += 2) {
+                if (value % i == 0)
+                    return false;
+            }
             return true;
         }
 
         boolean isEven(int value) {
-            return true;
+            return value % 2 == 0;
         }
 
         boolean isOdd(int value) {
-            return true;
+            return value % 2 == 0;
         }
 
         boolean isPrime(int value) {
+            if (value % 2 == 0)
+                return false;
+            for (int i = 3; i * i <= value; i += 2) {
+                if (value % i == 0)
+                    return false;
+            }
             return true;
         }
 
-        boolean isEven(MyInteger myInteger) {
-            return true;
+        static boolean isEven(MyInteger myInteger) {
+            return myInteger.value % 2 == 0;
         }
 
-        boolean isOdd(MyInteger myInteger) {
-            return true;
+        static boolean isOdd(MyInteger myInteger) {
+            return myInteger.value % 2 == 0;
         }
 
-        boolean isPrime(MyInteger myInteger) {
+        static boolean isPrime(MyInteger myInteger) {
+            if (myInteger.value % 2 == 0)
+                return false;
+            for (int i = 3; i * i <= myInteger.value; i += 2) {
+                if (myInteger.value % i == 0)
+                    return false;
+            }
             return true;
         }
 
         boolean equals(int value) {
-            return true;
+            return this.value == value;
         }
 
         boolean equals(MyInteger myInteger) {
-            return true;
+            return this.value == myInteger.value;
         }
 
         static int parseInt(char[] chars) {
-            return 0;
+            return parseInt(new String(chars));
         }
 
         static int parseInt(String string) {
-            return 0;
+            return Integer.parseInt(string);
         }
     }
 }
