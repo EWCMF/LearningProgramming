62cba9a871366f89bd22242dd51f3d95a3dbff7b
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-11-28 22:29:18 +0100

    Programmeringsopgaver kapitel 10.

---
+++ ProgrammingExercise/src/ProgrammingExercise10/Exercise3.java
@@ -0,0 +1,67 @@
+package ProgrammingExercise10;
+
+public class Exercise3 {
+    static class MyInteger {
+        private int value;
+
+        MyInteger(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        boolean isEven() {
+            return true;
+        }
+
+        boolean isOdd() {
+            return true;
+        }
+
+        boolean isPrime() {
+            return true;
+        }
+
+        boolean isEven(int value) {
+            return true;
+        }
+
+        boolean isOdd(int value) {
+            return true;
+        }
+
+        boolean isPrime(int value) {
+            return true;
+        }
+
+        boolean isEven(MyInteger myInteger) {
+            return true;
+        }
+
+        boolean isOdd(MyInteger myInteger) {
+            return true;
+        }
+
+        boolean isPrime(MyInteger myInteger) {
+            return true;
+        }
+
+        boolean equals(int value) {
+            return true;
+        }
+
+        boolean equals(MyInteger myInteger) {
+            return true;
+        }
+
+        static int parseInt(char[] chars) {
+            return 0;
+        }
+
+        static int parseInt(String string) {
+            return 0;
+        }
+    }
+}
