c2e4029b95803866616c44e2af7e1fe20293286f
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-03-28 08:42:37 +0100

    Unaffiliated module lagt ind med kort program.

--- Unaffiliated/src/romannumerals/Main.java
+++ Unaffiliated/src/romannumerals/Main.java
@@ -1,9 +1,8 @@
 package romannumerals;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
