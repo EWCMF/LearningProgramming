b57a77cdcf43829ea11706cac091ece41053be2e
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-05 21:00:41 +0200

    Programming exercise 18.3 started.

---
+++ ProgrammingExercise/src/ProgrammingExercise18/Exercise3.java
@@ -0,0 +1,11 @@
+package ProgrammingExercise18;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void dec2b(double x, int b, int n) {
+
+    }
+}
