d48d48be8b7c759115ce56fe41edbbbb55f8c86a
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-06 09:17:59 +0200

    Programming exercise 18.3 finished.

--- ProgrammingExercise/src/ProgrammingExercise18/Exercise3.java
+++ ProgrammingExercise/src/ProgrammingExercise18/Exercise3.java
@@ -1,11 +1,29 @@
 package ProgrammingExercise18;
 
 public class Exercise3 {
-    public static void main(String[] args) {
+    static String result = "0.";
 
+    public static void main(String[] args) {
+        dec2b(0.625, 3, 10);
     }
 
     public static void dec2b(double x, int b, int n) {
-
+        if (x < 0 || x >= 1) {
+            return;
+        }
+        n--;
+        double num1 = x * b;
+        int num2 = (int) (x * b);
+        double num3 = num1 - num2;
+        result =  result + num2;
+        if (n == 0) {
+            System.out.println(result);
+            return;
+        }
+        if (num1 == 1) {
+            System.out.println(result);
+            return;
+        }
+        dec2b(num3, b, n);
     }
 }
