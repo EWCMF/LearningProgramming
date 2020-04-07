178afd08844649408a2b68dffdc1ad84d2239b3d
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-07 22:35:49 +0200

    Programming exercise 18.4 started.

---
+++ ProgrammingExercise/src/ProgrammingExercise18/Exercise4.java
@@ -0,0 +1,14 @@
+package ProgrammingExercise18;
+
+// Combinations  refer  to  the  combination  of  n  things  taken  p  at  a  time  without
+// repetition. A recursive definition of C(n, p) is C(n, 0) = C(n, n) = 1, otherwise
+// C(n, p) = C(n-1, p) + C(n-1,p-1).
+// Write a method public static long C(long n, long p)
+// that computes and returns C(n, p)
+
+
+public class Exercise4 {
+    public static void main(String[] args) {
+
+    }
+}
